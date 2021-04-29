ad7ebb721980b951df0ebd6db64d548e91f8412b
Padilhovisck <36409740+Padilhovisck@users.noreply.github.com>
2021-04-29 09:58:36 -0300

    Update application.py

--- application.py
+++ application.py
@@ -3,6 +3,36 @@ app = Flask(__name__)
 
 import test
 
-@app.route("/")
-def hello():
-    return "Olá, moneynow ! "
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+
+
+# @app.route("/")
+# def hello():
+#     data = request.get_json()
+#     return "Olá, moneynow ! "
